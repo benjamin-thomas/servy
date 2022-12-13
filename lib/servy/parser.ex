27a0544a14c191c9845908cb9d906ce9d9625924
Benjamin <benjamin.guy.thomas@gmail.com>
2022-12-13 17:53:57 +0100

    Parse headers (14. recursion)

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,19 +4,33 @@ defmodule Servy.Parser do
   def parse(request, http_version) do
     [top, body] = String.split(request, "\n\n")
 
-    [request_line | _headers] = String.split(top, "\n")
+    [request_line | header_lines] = String.split(top, "\n")
 
     [method, path, ^http_version] = String.split(request_line, " ")
-    params = parse_params(body)
+    headers = parse_headers(header_lines, %{})
+    params = parse_params(headers["Content-Type"], body)
+
+    IO.inspect(header_lines)
 
     %Conv{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
 
-  def parse_params(params) do
+  def parse_params("application/x-www-form-urlencoded", params) do
     params |> String.trim() |> URI.decode_query()
+  end
+
+  def parse_params(_content_type, _params), do: %{}
+
+  def parse_headers([], headers), do: headers
+
+  def parse_headers([h | t], headers) do
+    [k, v] = String.split(h, ": ")
+    headers = Map.put(headers, k, v)
+    parse_headers(t, headers)
   end
 end
